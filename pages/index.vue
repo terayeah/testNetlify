576229cde0283e2273910c4ca6eda84db0d14658
terayeah <harrypoppopoo@gmail.com>
2021-08-12 13:27:46 +0900

    .

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,19 @@
 <template>
-  <Tutorial/>
+  <div>
+    <li v-for="post of posts" :key="post.slug">
+      <NuxtLink :to="post.slug">{{ post.title }}</NuxtLink>
+    </li>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+   async asyncData({ $content }) {
+    const posts = await $content("blog").fetch();
+
+    return {
+      posts,
+    };
+  },
+};
 </script>
